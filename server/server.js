d639a2bc633673fcf3e874f905abc207d619a74e
Alyssa Navarro <alyssa@webage.com>
2024-05-13 13:54:49 -0400

    get request for /api/planets

--- server/server.js
+++ server/server.js
@@ -10,3 +10,26 @@ dotenv.config();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
 const collectionName = process.env.MONGO_DB_COLLECTION;
+
+const app = express();
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
+const PORT = 3000;
+
+//get request for planets
+app.get("/api/planets", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const planets = await collection.find({}).toArray();
+    res.json(planets);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("No planets found!");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
